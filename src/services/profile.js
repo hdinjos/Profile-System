4600df93bb9f00bad803552e5645741fc01c5d63
hdinjos <hdinjos@gmail.com>
2021-03-21 06:05:28 +0700

    servise profile added

---
+++ src/services/profile.js
@@ -0,0 +1,45 @@
+import urlApi from './config';
+
+const subUrl = '/profile';
+
+const getProfile = async () => {
+  try {
+    const res = await urlApi.get(`${subUrl}/me`);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+const setProfile = async (payload) => {
+  try {
+    const res = await urlApi.post(`${subUrl}`, payload);
+  } catch (err) {
+    return err;
+  }
+};
+
+const setCareer = async (payload) => {
+  try {
+    const res = await urlApi.post(`${subUrl}/career`, payload);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+const setEducation = async (payload) => {
+  try {
+    const res = await urlApi.post(`${subUrl}/education`, payload);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+export {
+  getProfile,
+  setProfile,
+  setCareer,
+  setEducation
+};
