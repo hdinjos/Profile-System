ebec821ab58e6ee036712576449c09685442902f
hdinjos <hdinjos@gmail.com>
2021-03-21 23:09:39 +0700

    reconfig axios

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -1,20 +1,43 @@
 import axios from 'axios';
+import router from '../../router';
+// import Vue from 'vue';
 
 const urlApi = axios.create({
   baseURL: `${process.env.VUE_APP_CORS_SUPPORT}${process.env.VUE_APP_BASE_URL}`,
   withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
 });
 
 urlApi.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    urlApi.defaults.headers.common['Authorization'] = 'Bearer ' + token;
-    console.log(token);
-
+  function (config) {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = "Bearer " + token;
+      console.log(token);
+    }
     return config;
-  }, (error) => {
-    // Dxo something with request error
+  },
+  function (error) {
     return Promise.reject(error);
-  })
+  }
+);
+
+urlApi.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  if (error.response.status === 401) {
+    localStorage.removeItem('token');
+    router.push('/login');
+  }
+  return Promise.reject(error);
+});
+
 
 export default urlApi;
